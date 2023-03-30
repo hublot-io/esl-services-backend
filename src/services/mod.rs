637dc074ef6cee0296439d0f353ffd02f4c32150
nathan <nathan@hublot.io>
2023-03-30 17:32:01 +0200

    fix: conditional compilation

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -25,6 +25,14 @@ fn read_certificate(certificate_path: &str) -> Result<Vec<u8>, ClientError> {
     File::open(certificate_path)?.read_to_end(&mut buf)?;
     Ok(buf)
 }
+#[cfg(not(feature = "rustls-tls"))]
+fn get_identity(pem_content: Vec<u8>, pkcs8: Vec<u8>) -> Result<Identity, ClientError> {
+    Ok(Identity::from_pkcs8_pem(&pem_content, &pkcs8)?)
+}
+#[cfg(feature = "rustls-tls")]
+fn get_identity(pem_content: Vec<u8>, _pkcs8: Vec<u8>) -> Result<Identity, ClientError> {
+    Ok(Identity::from_pem(&pem_content)?)
+}
 
 /// Builds a configured reqwest http client.
 ///
@@ -35,20 +43,31 @@ pub fn build_client(
     certificate_pem: Option<String>,
     certificate_root: Option<String>,
     certificate_key: Option<String>,
-
 ) -> Result<Client, ClientError> {
-    let mut client_builder = ClientBuilder::new().use_native_tls();
+    let mut client_builder = ClientBuilder::new();
     if let Some(cs) = proxy_cs {
         debug!("Config contains a proxy connection string, adding it to the http client");
         let proxy_builder = Proxy::https(cs)?;
         client_builder = client_builder.proxy(proxy_builder)
     }
-    if let (Some(pem), Some(root), Some(key)) = (certificate_pem, certificate_root, certificate_key) {
+    if let (Some(pem), Some(root), Some(key)) = (certificate_pem, certificate_root, certificate_key)
+    {
         debug!("Config contains a certificate, adding it to the http client");
         let pem_content = read_certificate(&pem)?;
         let pkcs8 = read_certificate(&key)?;
         let root_content = read_certificate(&root)?;
-        let identity_builder = Identity::from_pkcs8_pem(&pem_content, &pkcs8)?;
+
+        #[cfg(feature = "rustls-tls")]
+        {
+            client_builder = client_builder.use_rustls_tls();
+        }
+        #[cfg(not(feature = "rustls-tls"))]
+        {
+            client_builder = client_builder.use_native_tls();
+        }
+
+        let identity_builder = get_identity(pem_content, pkcs8)?;
+
         client_builder = client_builder.identity(identity_builder);
         let cert = Certificate::from_pem(&root_content)?;
         client_builder = client_builder.add_root_certificate(cert);
